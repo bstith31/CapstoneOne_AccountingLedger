f4d754649c695ea726a800aee813dd2154fd1388
Brian <brian.stith0331@gmail.com>
2024-04-29 14:12:07 -0400

    created transaction class, started working out a deposit method

---
+++ src/main/java/com/ps/Transactions.java
@@ -0,0 +1,58 @@
+package com.ps;
+
+public class Transactions {
+
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+
+    public Transactions(String date, String time, String description, String vendor) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    @Override
+    public String toString() {
+        return "Transactions{" +
+                "date='" + date + '\'' +
+                ", time='" + time + '\'' +
+                ", description='" + description + '\'' +
+                ", vendor='" + vendor + '\'' +
+                '}';
+    }
+}
